65a32534fea006a2fbf3b17a8cc7ff2adc7103b2
Kiệt Lại Gia <laigiakiet@gmail.com>
2024-07-14 22:08:44 +0700

    Comment

--- kietttt/Program.cs
+++ kietttt/Program.cs
@@ -11,7 +11,7 @@ namespace kietttt
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new SanPham());
         }
     }
 }
--- kietttt/SanPham.cs
+++ kietttt/SanPham.cs
@@ -1,8 +1,8 @@
 namespace kietttt
 {
-    public partial class Form1 : Form
+    public partial class SanPham : Form
     {
-        public Form1()
+        public SanPham()
         {
             InitializeComponent();
         }
