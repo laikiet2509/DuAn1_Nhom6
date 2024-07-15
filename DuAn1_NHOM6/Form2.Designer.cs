aa4c0d208f58be3e107adbbd6860f8846c4f8848
Cong Long <conglong878@gmail.com>
2024-07-15 10:00:24 +0700

    Comment

--- DuAn1_NHOM6/Form2.Designer.cs
+++ DuAn1_NHOM6/Form2.Designer.cs
@@ -28,12 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            btnThem = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(73, 211);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(300, 188);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(342, 37);
+            label1.Name = "label1";
+            label1.Size = new Size(33, 20);
+            label1.TabIndex = 1;
+            label1.Text = "PCL";
+            // 
+            // btnThem
+            // 
+            btnThem.Location = new Point(484, 92);
+            btnThem.Name = "btnThem";
+            btnThem.Size = new Size(94, 29);
+            btnThem.TabIndex = 2;
+            btnThem.Text = "Them";
+            btnThem.UseVisualStyleBackColor = true;
+            btnThem.Click += btnThem_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnThem);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Name = "Form2";
+            Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private Button btnThem;
     }
 }
--- DuAn1_NHOM6/Form2.cs
+++ DuAn1_NHOM6/Form2.cs
@@ -16,5 +16,29 @@ namespace DuAn1_NHOM6
         {
             InitializeComponent();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView1 = new DataGridView();
+
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            string ten = tbxTen.Text;
+            string quocGia = tbxQuocGia.Text;
+            string namThanhLap = tbxNamTL.Text;
+            string HLV = tbxHLV.Text;
+
+            DataRow dataRow = dt.NewRow();
+
+            dataRow["Id"] = dt.Rows.Count + 1;
+            dataRow["Ten"] = tbxTen.Text;
+            dataRow["Quoc gia"] = tbxQuocGia.Text;
+            dataRow["Nam Thanh Lap"] = tbxNamTL.Text;
+            dataRow["HLV"] = tbxHLV.Text;
+            dt.Rows.Add(dataRow);
+            dataGridView1.DataSource = dt;
+        }
     }
 }
